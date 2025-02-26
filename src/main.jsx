8ec6a2621094cb8861d0ced1b85888afb1878dfd
icyclestick <icebebe111@gmail.com>
2025-02-27 02:34:02 +0800

    27/02/25 react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  // { path: "/about", element: <About /> },
+  // { path: "*", element: <NotFound /> }, // 404 page
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
